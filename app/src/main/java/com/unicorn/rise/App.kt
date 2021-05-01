c525574b113626eb0d5f48ed77e390791c278775
ivotai <ivolianer2015@163.com>
2021-05-01 12:45:58 +0800

    视频库初始化成功

--- app/src/main/java/com/unicorn/rise/App.kt
+++ app/src/main/java/com/unicorn/rise/App.kt
@@ -1,8 +1,12 @@
 package com.unicorn.rise
 
 import android.app.Application
+import android.content.Context
+import cn.tee3.avd.AVDEngine
+import cn.tee3.avd.ErrorCode
+import cn.tee3.avd.RoomInfo
+import com.blankj.utilcode.util.ToastUtils
 import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
 
@@ -15,6 +19,45 @@ class App : Application() {
 
             modules(appModule)
         }
+
+
+        init(this)
+
+    }
+
+    override fun onTerminate() {
+        super.onTerminate()
+        AVDEngine.instance().uninit()
+    }
+
+    private val listener = object : AVDEngine.Listener {
+        override fun onInitResult(result: Int) {
+            if (ErrorCode.AVD_OK != result) {
+                val err = "初始化失败 $result"
+                ToastUtils.showLong(err)
+                return
+            }
+        }
+
+        override fun onUninitResult(p0: Int) {
+        }
+
+        override fun onGetRoomResult(p0: Int, p1: RoomInfo?) {
+        }
+
+        override fun onFindRoomsResult(p0: Int, p1: MutableList<RoomInfo>?) {
+        }
+
+        override fun onScheduleRoomResult(p0: Int, p1: String?) {
+        }
+
+        override fun onCancelRoomResult(p0: Int, p1: String?) {
+        }
+    }
+
+
+    private fun init(context: Context) {
+        AVDEngine.instance().init(context, listener, serverUrl, appKey, secretKey)
     }
 
     private val appModule = module {
---
+++ app/src/main/java/com/unicorn/rise/const.kt
@@ -0,0 +1,13 @@
+package com.unicorn.rise
+
+//    public static final String
+//            tee3_avd_server = "121.196.210.198:8080";
+//      tee3_avd_server = "192.168.1.200";
+//    public static final String
+//            tee3_app_key    = "demo_access";
+//    public static final String
+//            tee3_secret_key = "demo_secret";
+
+const val serverUrl = "http://192.168.0.128"
+const val appKey = "demo_access"
+const val secretKey= "demo_secret"
