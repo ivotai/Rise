2b69548c865e2c306cfc9b76627820027a6bce5a
ivotai <ivolianer2015@163.com>
2021-05-01 08:22:12 +0800

    设置背景颜色

--- app/src/main/java/com/unicorn/rise/MainActivity.kt
+++ app/src/main/java/com/unicorn/rise/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.unicorn.rise
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import org.koin.android.ext.android.inject
+import android.view.View
+import com.ruffian.library.widget.RConstraintLayout
 
 class MainActivity : AppCompatActivity() {
 
-    private val baseUrl by inject<BaseUrl>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        baseUrl
         setContentView(R.layout.activity_main)
 
-
+        fun setBackgroundColorNormalArray() {
+            val helper = findViewById<RConstraintLayout>(R.id.root).helper
+            helper.backgroundColorNormalArray = intArrayOf(
+                Color.parseColor("#0094D2"),
+                Color.parseColor("#002564"),
+            )
+        }
+        setBackgroundColorNormalArray()
     }
+
 }
